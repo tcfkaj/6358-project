da2778560bbc79f97d920a3ddd517533c657395b
Steven Sun <31911818+stevenincode@users.noreply.github.com>
2018-11-15 00:22:51 -0600

    Update 3_r2_optimizer.R
    Can also use outlierTest function for dropping only the outliers

--- 3_r2_optimizer.R
+++ 3_r2_optimizer.R
@@ -13,11 +13,13 @@ looplist <- 1:nrow(clean_data)
 while (max(index_clean$`R-Sq`) < r2goal){
   for (i in looplist[-dropvar]){
     dropping <- c(-i, -dropvar)
-    model1 <- lm(Delta.price ~ ., data = index_clean[, -1], 
-                 subset = dropping)
+    model1 <- lm(Delta.price ~ ., data = index_clean[, -1], subset = dropping)
     index_clean[i, 'R-Sq'] <- summary(model1)['r.squared'][[1]]
   }
   dropvar <- c(dropvar, index_clean$Index[which(index_clean$`R-Sq`==max(index_clean$`R-Sq`[-dropvar]))])
+  # Or by outlierTest function for dropping only the outliers.
+  #model2 <- lm(Delta.price ~ ., data = index_clean[, -1], subset = -dropvar)
+  #dropvar <- c(dropvar, as.numeric(names(outlierTest(model2)[[1]])))
   print(max(index_clean$`R-Sq`))
   print(dropvar)
 }
