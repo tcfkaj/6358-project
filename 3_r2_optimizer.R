cce6e09c99e8f8acab3d41df2eeed261a22e44a4
Steven Sun <31911818+stevenincode@users.noreply.github.com>
2018-11-19 18:34:56 -0600

    Update 3_r2_optimizer.R
    Gives 28 outliers to be dropped to get 0.5 R squared.

--- 3_r2_optimizer.R
+++ 3_r2_optimizer.R
@@ -1,7 +1,7 @@
 clean_data <- read.csv('clean_data.csv', row.names = 1)
 
 # ===============
-r2goal <- 0.60 # R squared value we want
+r2goal <- 0.50 # R squared value we want
 # ===============
 
 index_clean <- cbind(1:nrow(clean_data), clean_data, rep(0, nrow(clean_data)))
@@ -16,10 +16,10 @@ while (max(index_clean$`R-Sq`) < r2goal){
     model1 <- lm(Delta.price ~ ., data = index_clean[, -1], subset = dropping)
     index_clean[i, 'R-Sq'] <- summary(model1)['r.squared'][[1]]
   }
-  dropvar <- c(dropvar, index_clean$Index[which(index_clean$`R-Sq`==max(index_clean$`R-Sq`[-dropvar]))])
-  # Or by outlierTest function for dropping only the outliers.
-  #model2 <- lm(Delta.price ~ ., data = index_clean[, -1], subset = -dropvar)
-  #dropvar <- c(dropvar, as.numeric(names(outlierTest(model2)[[1]])))
+  model2 <- lm(Delta.price ~ ., data = index_clean[, -1], subset = -dropvar)
+  dropvar <- c(dropvar, as.numeric(names(outlierTest(model2)[[1]])))
+  # Or dropping outliers that gives max R squared, but worse than the above method.
+  #dropvar <- c(dropvar, index_clean$Index[which(index_clean$`R-Sq`==max(index_clean$`R-Sq`[-dropvar]))])
   print(max(index_clean$`R-Sq`))
   print(dropvar)
 }
